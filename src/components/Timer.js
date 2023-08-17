7272732f4f68b9d9c89149ec915d52621c1f06d2
ashishjaiswal123 <jaiswalashish025@gmail.com>
2023-08-17 17:37:12 +0530

    react quiz app is devloped

---
+++ src/components/Timer.js
@@ -0,0 +1,23 @@
+import React, { useEffect } from "react";
+
+export default function Timer({ dispatch, secondRemaining }) {
+  const mins = Math.floor(secondRemaining / 60);
+  const secs = secondRemaining % 60;
+  useEffect(
+    function () {
+      const id = setInterval(function () {
+        dispatch({ type: "tick" });
+      }, 1000);
+
+      return () => clearInterval(id);
+    },
+    [dispatch]
+  );
+  return (
+    <div className="timer">
+      {mins < 10 && "0"}
+      {mins}:{secs < 10 && "0"}
+      {secs}
+    </div>
+  );
+}
